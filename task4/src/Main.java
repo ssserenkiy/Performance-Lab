b54ec0898bd2a11ac3f41c903d71ff3777b8d52e
ssserenkiy <140920370+ssserenkiy@users.noreply.github.com>
2023-07-31 04:28:16 +0400

    Add files via upload

---
+++ task4/src/Main.java
@@ -0,0 +1,65 @@
+import java.io.IOException;
+
+/*
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+*/
+
+public class Main {
+
+    public static void main(String[] args) throws IOException
+    {
+
+        /*
+        List<String> listOfStrings = new ArrayList<String>();
+        BufferedReader bf = new BufferedReader(new FileReader("task4input.txt"));
+        String line = bf.readLine();
+        while (line != null) {
+            listOfStrings.add(line);
+            line = bf.readLine();
+        }
+        bf.close();
+        String[] array = listOfStrings.toArray(new String[0]);
+        /*
+        for (String str : array) {
+            System.out.println(str);
+        }
+        */
+
+
+        int[] numbers = new int[args.length];
+        for (int j = 0; j < args.length; j++) {
+            numbers[j] = Integer.parseInt(args[j]);
+        }
+        var sum = 0.0;
+        for (int j = 0; j < numbers.length; j++) {
+            sum += numbers[j];
+        }
+        var average = sum / numbers.length;
+        int amountLess = 0;
+        int amountMore = 0;
+        for (int j = 0; j < numbers.length; j++) {
+            if(numbers[j] > average){
+                amountMore++;
+            } else amountLess++;
+        }
+        //    System.out.println("Меньших среднего: " + amountLess);
+        //    System.out.println("Больших среднего: " + amountMore);
+
+        if (amountLess > amountMore){
+            average = Math.floor(average);
+        } else average = Math.ceil(average);
+        //    System.out.println("Среднее массива: " + average);
+        int amountOperation = 0;
+
+        for (int j = 0; j < numbers.length; j++) {
+            amountOperation += Math.abs(numbers[j] - average);
+        }
+        //    System.out.println(Arrays.toString(numbers));
+        System.out.println(/*"Количество операций: " + */amountOperation);
+
+    }
+}
